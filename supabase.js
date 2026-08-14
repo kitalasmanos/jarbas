cfeff4976d9fdefe66ddf9c511398c6c89ba6bef
Kitalasmanos <62261034+kitalasmanos@users.noreply.github.com>
2026-08-14 21:25:21 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,10 +1,39 @@
 const SUPABASE_URL = "https://unoynpwkpvmyzdoyyvov.supabase.co";
 
-const SUPABASE_KEY = "A_TUA_CHAVE";
+const SUPABASE_KEY = "sb_publishable_gH615f9gg0kVHqVTfcKDdw_NRPhHG0I";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
 
-alert("SUPABASE LIGADO");
+async function testarSupabase() {
+
+    const { data, error } =
+        await supabaseClient
+        .from("pessoas")
+        .insert([
+            {
+                nome: "Pessoa A"
+            }
+        ])
+        .select();
+
+    if (error) {
+
+        alert(
+            "Erro: " +
+            error.message
+        );
+
+        return;
+    }
+
+    alert(
+        "Pessoa criada com sucesso!"
+    );
+
+    console.log(data);
+}
+
+testarSupabase();

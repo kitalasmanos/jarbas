e3abdfafc847d686842eada38581e276c76a3908
Kitalasmanos <62261034+kitalasmanos@users.noreply.github.com>
2026-08-14 21:43:39 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://unoynpwkpvmyzdoywyov.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://unoynpwkpvmyzdoywyov.supabase.co";
 const SUPABASE_KEY = "sb_publishable_gH615f9gg0kVHqVTfcKDdw_NRPhHG0I";
 
 const supabaseClient = supabase.createClient(
